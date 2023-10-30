227aae6351acbcf7dbcfeae909a933c2a3d83b21
Lucas Valença <lucas@valenca.io>
2023-10-30 19:41:01 -0400

    sigasia website ready

--- shadowcompositing/script.js
+++ shadowcompositing/script.js
@@ -1,5 +1,27 @@
-var fg, slider, slider_bt;
+var mybutton;
 
+window.onload = function() {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+
+    mybutton = document.getElementById("btn-back-to-top");
+    mybutton.addEventListener("click", backToTop);
+
+    window.onscroll = function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+          } else mybutton.style.display = "none";
+    };
+}
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+var fg, slider, slider_bt;
 function moveDivisor(id) {
     slider = document.getElementById("slider" + id);
     slider_bt = document.getElementById("sliderBt" + id);
