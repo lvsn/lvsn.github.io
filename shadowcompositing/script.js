478ecf626a3fae31bd6bace358b2b28ec9e8d565
Lucas Valença <lucas@valenca.io>
2023-10-30 17:36:54 -0400

    sigasia website updates

--- shadowcompositing/script.js
+++ shadowcompositing/script.js
@@ -1,12 +1,10 @@
 var fg, slider, slider_bt;
 
-window.onload = function(e) {
-    fg = document.getElementById("fg");
-    slider = document.getElementById("slider");
-    slider_bt = document.getElementById("sliderBt");
-}
+function moveDivisor(id) {
+    slider = document.getElementById("slider" + id);
+    slider_bt = document.getElementById("sliderBt" + id);
+    fg = document.getElementById("fg" + id);
 
-function moveDivisor() {
     fg.style['width'] = slider.value+"%";
     slider_bt.style['left'] = "calc("+slider.value+"% - 18px)";
 }
